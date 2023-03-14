a055840887a772e83d41693a4517ea44e8c88a21
ShreeshanthiAchar <124127662+ShreeshanthiAchar@users.noreply.github.com>
2023-03-14 12:22:07 +0530

    Add files via upload

---
+++ AssignmentTwo/Js/main2.js
@@ -0,0 +1,9 @@
+$('.responsive').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    prevArrow: false,
+    nextArrow: false,
+  });
