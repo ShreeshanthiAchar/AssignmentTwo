5273bfd7957babb1ba0d2b45443c9edcedb91b46
ShreeshanthiAchar <124127662+ShreeshanthiAchar@users.noreply.github.com>
2023-03-17 17:41:20 +0530

    Add files via upload

---
+++ AssignmentTwo/Js/main2.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('.responsive').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    prevArrow: false,
+    nextArrow: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
