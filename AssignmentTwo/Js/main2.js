f5f5bba9a96feb3be95e4df16293435ff99f2a7e
ShreeshanthiAchar <124127662+ShreeshanthiAchar@users.noreply.github.com>
2023-03-17 17:38:51 +0530

    Delete AssignmentTwo/Js directory

--- AssignmentTwo/Js/main2.js
+++
@@ -1,9 +0,0 @@
-$('.responsive').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    dots: false,
-    prevArrow: false,
-    nextArrow: false,
-  });
